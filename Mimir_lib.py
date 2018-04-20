304dac925e3caa08e049193399848498a5012853
PierrickLP <pierrickleplenier@gmail.com>
2018-04-20 14:18:21 +0200

    Add masks and points display

--- Mimir_lib.py
+++ Mimir_lib.py
@@ -47,34 +47,52 @@ class Fd_data:
         scales_indexes = [((x + plane_nb) % 3) + 1 for x in [1, 2]]
 
         image = Image.fromarray(converted)
-
+        image = self.__draw_points_masks(image, img_nb, plane_nb, slice_nb)
         image = image.rotate(90, expand=True)
         pixdim = self.header['pixdim']
         new_width = int(round(image.width*pixdim[min(scales_indexes)]))
         new_height = int(round(image.height*pixdim[max(scales_indexes)]))
+        
+        
         image = image.resize((new_width, new_height))
-        image = self.__draw_points_masks(image, img_nb, plane_nb, slice_nb)
         return image, (pixdim[min(scales_indexes)], pixdim[max(scales_indexes)])
-    
+
     def __draw_points_masks(self, image, img_nb, plane_nb, slice_nb):
+        image = image.convert('RGBA')
         image_draw = ImageDraw.Draw(image)
+        #image_draw.line((100,200, 150, 300), fill=128, width=3)
+        
         for mask in self.masks:
-            image_draw.polygon([a[:plane_nb]+a[plane_nb+1:] for a in mask.points if a[plane_nb] == slice_nb], fill=128)
-        image_draw.point([a[:plane_nb]+a[plane_nb+1:3] for a in self.points if a[3] == img_nb and a[plane_nb] == slice_nb], fill=32)
+            mask_points = []
+            for a in mask.points:
+                if a[plane_nb] == slice_nb:
+                    temp_list = a[:plane_nb]+a[plane_nb+1:]
+                    temp_list.reverse()
+                    mask_points.extend(temp_list)
+            if len(mask_points) >= 4:
+                image_draw.polygon(tuple(mask_points), fill=(255,0,0,255))
+        points_position = []
+        for a in self.points:
+            if a[3] == img_nb and a[plane_nb] == slice_nb:
+                temp_list = a[:plane_nb]+a[plane_nb+1:3]
+                temp_list.reverse()
+                print(slice_nb,temp_list)
+                image_draw.point(tuple(temp_list), fill=(255,0,0,255))
         return image
 
     def add_point(self, point):
-        if len(point) == 4:
+        if len(point) == 4 and point not in self.points:
             self.points.append(point)
     
     def delete_point(self, index):
         if index < len(self.points) and index >= 0:
             del self.points[index]
     
-    def add_point_to_mask(self, index, point):
-        if index >= len(self.masks):
-            self.masks[index] = Mask()
-        self.masks[index].add_point(point)
+    def add_point_to_mask(self, point, index):
+        while index >= len(self.masks):
+            self.masks.append(Mask())
+        if len(point) == 3 and point not in self.masks[index].points:
+            self.masks[index].add_point(point)
 
     def delete_point_from_mask(self, index, pointIndex):
         self.masks[index].delete_point(pointIndex)
@@ -108,27 +126,38 @@ class Mask:
         self.index_freeze = -1
         self.value_freeze = -1
     
-    def add_point(point):
+    def add_point(self, point):
         if len(point) == 3:
-            if index_freeze != -1 and point[index_freeze] != value_freeze :
+            if self.index_freeze != -1 and point[self.index_freeze] != self.value_freeze :
                 return
+            print("point",point)
+            print("self.points",self.points)
             self.points.append(point)
-            wc = [{}, {}, {}]
-            t = []
-            for b in a:
-                for i, c in enumerate(b):
-                    wc[i][c] = wc[i][c] + 1 if c in wc[i] else 1
-            for k,v in enumerate(wc):
-                for a in v:
-                    if v[a] >= 2:
-                        t.append((k, a))
-            if len(t) == 0:
-                del self.points[len(self.points)]
-                return
-            if len(t) == 1:
-                self.index_freeze = t[0][0]
-                self.value_freeze = t[0][1]           
-    
-    def delete_point(index):
+            print("self.points",self.points)
+            if len(self.points) > 1:
+                wc = [{}, {}, {}]
+                t = []
+                for b in self.points:
+                    print("b",b)
+                    for i, c in enumerate(b):
+                        print("i, c",i,c)
+                        wc[i][c] = wc[i][c] + 1 if c in wc[i] else 1
+                for k,v in enumerate(wc):
+                    print("k,v",k,v)
+                    for a in v:
+                        print("a, v[a]",a, v[a])
+                        if v[a] >= 2:
+                            t.append((k, a))
+                print("t",t)
+                if len(t) == 0:
+                    print("del self.points",self.points)
+                    del self.points[len(self.points)-1]
+                    return
+                if len(t) == 1:
+                    self.index_freeze = t[0][0]
+                    self.value_freeze = t[0][1]           
+        print("self.points",self.points)
+
+    def delete_point(self, index):
         if index < len(self.points) and index >= 0:
             del self.points[index]
--- POC_CLI.py
+++
@@ -1,6 +0,0 @@
-import sys
-import POC_LIB
-
-
-test = POC_LIB.nifty_parser(sys.argv[1])
-POC_LIB.save_slice(POC_LIB.format_image(*test), sys.argv[2])
