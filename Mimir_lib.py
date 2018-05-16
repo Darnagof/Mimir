bbf162e2d1c356f47e8ea35d1cf27a4f0eebc460
PierrickLP <pierrickleplenier@gmail.com>
2018-05-16 11:40:44 +0200

    Add color selection

--- Mimir_lib.py
+++ Mimir_lib.py
@@ -16,27 +16,37 @@ class Fd_data:
         self.points = []
         self.masks = []
 
-    def _get_contrast_min(self):
+    def get_contrast_min(self):
         return self.contrast_min
 
-    def _get_contrast_max(self):
+    def get_contrast_max(self):
         return self.contrast_max
     
-    def _get_shape(self):
+    def get_shape(self):
         return self.shape
 
     def get_slice(self, img_nb, plane_nb, slice_nb, contrast_min, contrast_max, colormap):
         # the slice(None) index will take an entire dimension, so using 2 of them and a number will reduce the
         # dimensions of the original array by one if the image is 3D
+        print("_____________________________")
         slice_range = [slice(None)] * 3
+        print(slice_range)
+        print("---")
         slice_range[plane_nb] = slice_nb
+        print(slice_range)
+        print("---")
         slice_range = tuple(slice_range)
-
+        print(slice_range)
+        print("---")
         # if the original image is 4D, we need to further reduce the number of dimensions by selecting a 3D image
         if len(self.shape) == 4: slice_range = slice_range + (img_nb,)
-    
+        print(slice_range)
+        
+        print("---")
         # after the 2D plane has been extracted, the contrast must be changed according to the contrast sliders' values
         plane = numpy.copy(self.data[slice_range])
+        print(plane)
+        print("---")
         plane[plane < contrast_min] = contrast_min
         plane[plane > contrast_max] = contrast_max
 
@@ -49,7 +59,7 @@ class Fd_data:
         image = Image.fromarray(converted)
         if colormap:
             image = set_colormap(image, colormap)
-        image = self.__draw_points_masks(image, img_nb, plane_nb, slice_nb)
+        image = self._draw_points_masks(image, img_nb, plane_nb, slice_nb)
         image = image.rotate(90, expand=True)
         pixdim = self.header['pixdim']
         new_width = int(round(image.width*pixdim[min(scales_indexes)]))
@@ -59,10 +69,9 @@ class Fd_data:
         image = image.resize((new_width, new_height))
         return image, (pixdim[min(scales_indexes)], pixdim[max(scales_indexes)])
 
-    def __draw_points_masks(self, image, img_nb, plane_nb, slice_nb):
-        image = image.convert('RGBA')
-        image_draw = ImageDraw.Draw(image)
-        #image_draw.line((100,200, 150, 300), fill=128, width=3)
+    def _draw_points_masks(self, image, img_nb, plane_nb, slice_nb):
+        image = image.convert('RGB')
+        image_draw = ImageDraw.Draw(image, 'RGBA')
         
         for mask in self.masks:
             mask_points = []
@@ -72,29 +81,29 @@ class Fd_data:
                     temp_list.reverse()
                     mask_points.extend(temp_list)
             if len(mask_points) >= 4:
-                image_draw.polygon(tuple(mask_points), fill=(255,0,0,255))
+                color = mask.get_color() if mask.get_color() else (255,0,0,255)
+                image_draw.polygon(tuple(mask_points), fill=color)
         points_position = []
         for a in self.points:
             if a[3] == img_nb and a[plane_nb] == slice_nb:
+                color = tuple(a[:4]) if a[:4] else (255,0,0,255)
                 temp_list = a[:plane_nb]+a[plane_nb+1:3]
                 temp_list.reverse()
-                print(slice_nb,temp_list)
-                image_draw.point(tuple(temp_list), fill=(255,0,0,255))
+                image_draw.point(tuple(temp_list), fill=color)
         return image
 
-    def add_point(self, point):
+    def add_point(self, point, color=None):
         if len(point) == 4 and point not in self.points:
-            self.points.append(point)
+            self.points.append(point+color)
     
     def delete_point(self, index):
         if index < len(self.points) and index >= 0:
             del self.points[index]
     
-    def add_point_to_mask(self, point, index):
+    def get_mask(self, index):
         while index >= len(self.masks):
             self.masks.append(Mask())
-        if len(point) == 3 and point not in self.masks[index].points:
-            self.masks[index].add_point(point)
+        return self.masks[index]
 
     def delete_point_from_mask(self, index, pointIndex):
         self.masks[index].delete_point(pointIndex)
@@ -108,6 +117,15 @@ class Fd_data:
             l_points, l_masks = pickle.load(fp)
             self.points.extend(l_points)
             self.masks.extend(l_masks)
+    
+    def save(self, save_path):
+        shape_3d = self.img.shape[0:3]
+        rgb_dtype = numpy.dtype([('R', 'u1'), ('G', 'u1'), ('B', 'u1')])
+        ras_pos = self.img.copy().view(dtype=rgb_dtype).reshape(shape_3d)  # copy used to force fresh internal structure
+        ni_img = nibabel.Nifti1Image(self.img, numpy.eye(4))
+        #new_img = nibabel.Nifti1Image(self.data, self.img.affine, self.header)
+        
+        nibabel.save(ni_img, save_path)
 
 def set_colormap(image, color):
     cmap = cm.get_cmap(color)
@@ -127,38 +145,35 @@ class Mask:
         self.points = []
         self.index_freeze = -1
         self.value_freeze = -1
-    
+        self.color = None
+
+    def set_color(self, color):
+        self.color = tuple(color)
+
+    def get_color(self):
+        return self.color
+
     def add_point(self, point):
-        if len(point) == 3:
+        if len(point) == 3 and point not in self.points:
             if self.index_freeze != -1 and point[self.index_freeze] != self.value_freeze :
                 return
-            print("point",point)
-            print("self.points",self.points)
             self.points.append(point)
-            print("self.points",self.points)
             if len(self.points) > 1:
                 wc = [{}, {}, {}]
                 t = []
                 for b in self.points:
-                    print("b",b)
                     for i, c in enumerate(b):
-                        print("i, c",i,c)
                         wc[i][c] = wc[i][c] + 1 if c in wc[i] else 1
                 for k,v in enumerate(wc):
-                    print("k,v",k,v)
                     for a in v:
-                        print("a, v[a]",a, v[a])
                         if v[a] >= 2:
                             t.append((k, a))
-                print("t",t)
                 if len(t) == 0:
-                    print("del self.points",self.points)
                     del self.points[len(self.points)-1]
                     return
                 if len(t) == 1:
                     self.index_freeze = t[0][0]
                     self.value_freeze = t[0][1]           
-        print("self.points",self.points)
 
     def delete_point(self, index):
         if index < len(self.points) and index >= 0:
